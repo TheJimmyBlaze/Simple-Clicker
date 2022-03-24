9bde6be6657ed8f613ca58e2631be1acdd3a2625
TheJimmyBlaze <TheJimmyBlaze@gmail.com>
2022-03-24 13:34:27 +1300

    Cookies save on update

--- src/features/cookies/useCookies.js
+++ src/features/cookies/useCookies.js
@@ -1,12 +1,25 @@
-import { useCallback } from 'react';
+import { useRef, useCallback } from 'react';
 import moment from 'moment';
 import Cookies from 'universal-cookie';
 
-export const useCookies = saveStateName => {
+const minutesBetweenSaves = 0.5;
 
+export const useCookies = (saveStateName) => {
+
+    let lastUpdateTime = useRef(moment());
     const cookies = new Cookies();
 
-    const setSaveState = useCallback(saveState => (
+    const setSaveState = useCallback(saveState => {
+
+        const nextSaveTime = moment(lastUpdateTime.current).add(minutesBetweenSaves, 'minutes');
+        console.log(moment());
+        console.log(nextSaveTime);
+        if (moment() < nextSaveTime) {
+            return;
+        }
+
+        console.log('save');
+        lastUpdateTime.current = Date.now();
         cookies.set(saveStateName, 
             JSON.stringify(saveState), 
             {
@@ -15,7 +28,7 @@ export const useCookies = saveStateName => {
                 sameSite: 'strict'
             }
         )
-    ), [cookies, saveStateName]);
+    }, [cookies, lastUpdateTime, saveStateName, minutesBetweenSaves]);
 
     const getSaveState = useCallback(() => (
         cookies.get(saveStateName)
